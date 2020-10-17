25a228f939e24e057aed68ea007f5ec3b4fd8b0c
Ram Maheshwari <ram991011@gmail.com>
2020-10-17 23:08:45 +0530

    backup

---
+++ common.js
@@ -0,0 +1,12 @@
+var myNav = document.querySelector('.main-nav')
+window.onscroll = function () {
+  console.log('nooo', myNav)
+  if (
+    document.body.scrollTop >= 200 ||
+    document.documentElement.scrollTop >= 200
+  ) {
+    myNav.classList.add('main-nav-inv')
+  } else {
+    myNav.classList.remove('main-nav-inv')
+  }
+}
