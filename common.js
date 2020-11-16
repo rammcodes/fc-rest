59813cbecca02ea3e2e0d7ce49c7f8a597146008
Ram Maheshwari <ram991011@gmail.com>
2020-11-16 23:14:16 +0530

    backup

--- common.js
+++ common.js
@@ -9,3 +9,15 @@ window.onscroll = function () {
     myNav.classList.remove('main-nav-inv')
   }
 }
+
+let menuClose = document.querySelector('.menu-close')
+let navMenu = document.querySelector('.nav-menu')
+let fullScrMenu = document.querySelector('.fl-scr-menu')
+
+menuClose.addEventListener('click', () => {
+  fullScrMenu.classList.add('none')
+})
+
+navMenu.addEventListener('click', () => {
+  fullScrMenu.classList.remove('none')
+})
